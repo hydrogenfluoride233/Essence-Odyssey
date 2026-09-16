efebf7b1be754c3d48e6c9a757eeee0b59e64a00
hydrogenfluoride233 <hydrogenfluoride233@users.noreply.github.com>
2026-09-16 20:40:59 +0800

    EMI 清理清单补充：导管、创造流体储罐

--- kubejs/client_scripts/emi/hidden.js
+++ kubejs/client_scripts/emi/hidden.js
@@ -5,7 +5,9 @@
 //要隐藏的物品：连它的各种 NBT 变体一起隐藏
 //格式就是注册名，一行一个，加注释说明为什么
 const HiddenEntries = [
-    "enderio:soul_vial",   //灵魂瓶：装了不同灵魂会各占一个条目
+    "enderio:soul_vial",           //灵魂瓶：装了不同灵魂会各占一个条目
+    "enderio:conduit",             //导管：各种导管/伪装也各占一个条目
+    "create:creative_fluid_tank",  //创造流体储罐：同样带流体变体
 ]
 
 RecipeViewerEvents.removeEntries("item", event => {
