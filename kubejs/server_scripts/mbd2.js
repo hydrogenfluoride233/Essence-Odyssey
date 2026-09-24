39c491e16a25c03e6bc797d148576b172eb791a1
hydrogenfluoride233 <hydrogenfluoride@outlook.com>
2026-09-24 16:14:50 +0800

    工业灌注室持续时间

--- kubejs/server_scripts/mbd2.js
+++ kubejs/server_scripts/mbd2.js
@@ -66,7 +66,7 @@ ServerEvents.recipes(event=>{
     ImbuementList.forEach(([input,output,source,name])=>
         event.recipes.mbd2.industrial_imbuement()
             .id(`mbd2:industrial_imbuement/${name}`)
-            .duration(100)
+            .duration(50)
             .inputItems(input)
             .inputSource(source)
             .outputItems(output)
